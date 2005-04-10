c4587275a0002d2eb79bb93689c8569efcac9e8d
dullmann <dullmann@b113d865-ab28-0410-9ae9-d2690f63996d>
2005-04-10 12:21:24 +0000

    installed server side interceptors

---
+++ IIOPNet/IntegrationTests/InterceptorTests/InvocationScenarios/TestServer/Interceptors.cs
@@ -0,0 +1,159 @@
+/* Interceptors.cs
+ *
+ * Project: IIOP.NET
+ * IntegrationTests
+ *
+ * WHEN      RESPONSIBLE
+ * 10.04.05  Dominic Ullmann (DUL), dominic.ullmann -at- elca.ch
+ *
+ * Copyright 2003 ELCA Informatique SA
+ * Av. de la Harpe 22-24, 1000 Lausanne 13, Switzerland
+ * www.elca.ch
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+using System;
+using System.Reflection;
+using System.Collections;
+using Ch.Elca.Iiop;
+using Ch.Elca.Iiop.Idl;
+using Ch.Elca.Iiop.Services;
+using Ch.Elca.Iiop.Interception;
+using omg.org.CosNaming;
+using omg.org.CORBA;
+using omg.org.PortableInterceptor;
+
+namespace Ch.Elca.Iiop.IntegrationTests {
+
+
+    public enum OutPathResult {
+        NotCalled, Reply, Exception, Other
+    }
+
+
+    /// <summary>
+    /// adds the test interceptors.
+    /// </summary>
+    public class TestInterceptor : ServerRequestInterceptor {
+
+
+        private string m_name;
+        private string m_testId;
+        private bool m_invokedOnInPathReceiveSvcContext = false;
+        private bool m_invokedOnInPathReceive = false;
+        private OutPathResult m_outPathResult = OutPathResult.NotCalled;
+        private Exception m_throwExceptionOutPath = null;
+        private Exception m_throwExceptionInPathReceive = null;
+        private Exception m_throwExceptionInPathReceiveSvcContext = null;
+
+
+        public TestInterceptor(string name, string testId) {
+            m_name = name;
+            m_testId = testId;
+        }
+
+        public string Name {
+            get {
+                return m_name;
+            }
+        }
+
+        /// <summary>for debugging purposes</summary>
+        public string TestId {
+            get {
+                return m_testId;
+            }
+        }
+
+        public bool InvokedOnInPathReceiveSvcContext {
+            get {
+                return m_invokedOnInPathReceiveSvcContext;
+            }
+        }
+
+        public bool InvokedOnInPathReceive {
+            get {
+                return m_invokedOnInPathReceive;
+            }
+        }
+
+        public OutPathResult OutPathResult {
+            get {
+                return m_outPathResult;
+            }
+        }
+
+        public void ClearInvocationHistory() {
+            m_invokedOnInPathReceiveSvcContext = false;
+            m_invokedOnInPathReceive = false;
+            m_outPathResult = OutPathResult.NotCalled;
+            m_throwExceptionOutPath = null;
+            m_throwExceptionInPathReceive = null;
+            m_throwExceptionInPathReceiveSvcContext = null;
+        }
+
+        public void receive_request_service_contexts(ServerRequestInfo ri) {
+            m_invokedOnInPathReceiveSvcContext = true;
+            if (m_throwExceptionInPathReceiveSvcContext != null) {
+                throw m_throwExceptionInPathReceiveSvcContext;
+            }
+        }
+                
+        public void receive_request(ServerRequestInfo ri) {
+            m_invokedOnInPathReceive = true;
+            if (m_throwExceptionInPathReceive != null) {
+                throw m_throwExceptionInPathReceive;
+            }            
+        }
+        
+        public void send_reply(ServerRequestInfo ri) {
+            m_outPathResult = OutPathResult.Reply;
+            if (m_throwExceptionOutPath != null) {
+                throw m_throwExceptionOutPath;
+            }
+        }
+        
+        public void send_exception(ServerRequestInfo ri) {
+            m_outPathResult = OutPathResult.Exception;
+            if (m_throwExceptionOutPath != null) {
+                throw m_throwExceptionOutPath;
+            }
+        }
+        
+        public void send_other(ServerRequestInfo ri) {
+            m_outPathResult = OutPathResult.Other;
+            if (m_throwExceptionOutPath != null) {
+                throw m_throwExceptionOutPath;
+            }
+        }
+
+        public void SetExceptionOnInPathSvcContext(Exception ex) {
+            m_throwExceptionInPathReceiveSvcContext = ex;
+        }
+
+        public void SetExceptionOnInPathRequest(Exception ex) {
+            m_throwExceptionInPathReceive = ex;
+        }
+
+        public void SetExceptionOnOutPath(Exception ex) {
+            m_throwExceptionOutPath = ex;
+        }
+
+    }
+
+
+}
