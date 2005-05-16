233fbc32cba1c82b6bbed27caeafd126d287ab17
dullmann <dullmann@b113d865-ab28-0410-9ae9-d2690f63996d>
2005-05-16 14:24:47 +0000

    corrected to work also with the gui runner

--- branches/iiop-net-1-8-0-async_trrefact/IIOPNet/IntegrationTests/BidirGiop/JacOrbClient/TestClient/TestClient.java
+++ branches/iiop-net-1-8-0-async_trrefact/IIOPNet/IntegrationTests/BidirGiop/JacOrbClient/TestClient/TestClient.java
@@ -12,34 +12,36 @@ import java.util.Properties;
  */
 public class TestClient extends TestCase {
 
-    private static TestService s_testService;
-    private static org.omg.CORBA.ORB s_orb;
-    private static CallBack s_cb;
-    private static ClientCallbackImpl s_cbServant;
-    private static CallbackIntIncrementer s_cbIntIncr;
+    private TestService m_testService;
+    private org.omg.CORBA.ORB m_orb;
+    private CallBack m_cb;
+    private ClientCallbackImpl m_cbServant;
+    private CallbackIntIncrementer m_cbIntIncr;
 
     public static void main (String[] args) {
         try {
-            Properties props = new Properties();
-            props.put( "org.omg.PortableInterceptor.ORBInitializerClass.bidir_init",
-                       "org.jacorb.orb.giop.BiDirConnectionInitializer" );
-            props.put("ORBInitRef.NameService", "corbaloc::127.0.0.1:8087/NameService");
-            s_orb = org.omg.CORBA.ORB.init( args, props );
-
-            ResolveTestService();
-            CreateCallbackObject();        
-
             junit.textui.TestRunner.run (suite());
         } catch (Exception ex) {
             System.out.println("error while running test: " + ex);
         }
     }
 
-    private static void CreateCallbackObject() throws Exception {
-        POA rootPoa = (POA) s_orb.resolve_initial_references( "RootPOA" );
+    private void InitOrb() throws Exception {
+        Properties props = new Properties();
+        props.put( "org.omg.PortableInterceptor.ORBInitializerClass.bidir_init",
+                   "org.jacorb.orb.giop.BiDirConnectionInitializer" );
+        props.put("ORBInitRef.NameService", "corbaloc::127.0.0.1:8087/NameService");
+        m_orb = org.omg.CORBA.ORB.init( new String[0], props );
+
+        ResolveTestService();
+        CreateCallbackObject();                
+    }    
+
+    private void CreateCallbackObject() throws Exception {
+        POA rootPoa = (POA) m_orb.resolve_initial_references( "RootPOA" );
 
         // create orb with bidir policy
-        Any any = s_orb.create_any();
+        Any any = m_orb.create_any();
         BidirectionalPolicyValueHelper.insert( any, BOTH.value );
 
         Policy[] policies = new Policy[4];
@@ -52,38 +54,40 @@ public class TestClient extends TestCase {
         policies[2] = 
             rootPoa.create_implicit_activation_policy( ImplicitActivationPolicyValue.IMPLICIT_ACTIVATION );
 
-        policies[3] = s_orb.create_policy( BIDIRECTIONAL_POLICY_TYPE.value,
+        policies[3] = m_orb.create_policy( BIDIRECTIONAL_POLICY_TYPE.value,
                                            any );
 
         POA bidirPoa = rootPoa.create_POA( "BiDirPOA",
                                            rootPoa.the_POAManager(),
                                            policies );
         bidirPoa.the_POAManager().activate();
-        System.out.println("register callback servants");
-        s_cbServant = new ClientCallbackImpl();
+        m_cbServant = new ClientCallbackImpl();
 
-        s_cb = CallBackHelper.narrow( 
-                                bidirPoa.servant_to_reference( s_cbServant ));
+        m_cb = CallBackHelper.narrow( 
+                                bidirPoa.servant_to_reference( m_cbServant ));
 
         ClientCallbackIntIncrementerImpl cbIntIncrServant = 
                                              new ClientCallbackIntIncrementerImpl();
-        s_cbIntIncr = CallbackIntIncrementerHelper.narrow(
+        m_cbIntIncr = CallbackIntIncrementerHelper.narrow(
                                                        bidirPoa.servant_to_reference(cbIntIncrServant));
     }
 
-    private static void ResolveTestService() throws Exception {
+    private void ResolveTestService() throws Exception {
         org.omg.CosNaming.NameComponent[] serviceName = 
             new org.omg.CosNaming.NameComponent[1];
         serviceName[0] = new NameComponent("test", "");
         org.omg.CosNaming.NamingContext nc = 
             org.omg.CosNaming.NamingContextHelper.narrow(
-                s_orb.resolve_initial_references( "NameService" ));        
+                m_orb.resolve_initial_references( "NameService" ));        
 
         org.omg.CORBA.Object service = nc.resolve(serviceName);
-        s_testService = TestServiceHelper.narrow(service);
+        m_testService = TestServiceHelper.narrow(service);
     }
 
     protected void setUp() throws Exception {
+        if (m_orb == null) {
+            InitOrb(); // the first time, setup orb
+        }
     }
 
     protected void tearDown() {
@@ -96,21 +100,21 @@ public class TestClient extends TestCase {
     public void testWithoutCallback() throws Exception {
         // without callback
         byte arg1 = 1;
-        byte result1 = s_testService.TestIncByte(arg1);
+        byte result1 = m_testService.TestIncByte(arg1);
         assertEquals("wrong result 1", arg1 + 1, result1);
     }
 
     public void testCallbackArgument() throws Exception {
         // for the following callbacks, should use already existing connection for callback (bidir)
         String arg2 = "test";        
-        s_testService.string_callback(s_cb, arg2);
-        assertEquals("wrong result 2", arg2, s_cbServant.GetMsg());
+        m_testService.string_callback(m_cb, arg2);
+        assertEquals("wrong result 2", arg2, m_cbServant.GetMsg());
     }
 
     public void testCallbackPrereg() throws Exception {
         int arg3 = 3;        
-        s_testService.RegisterCallbackIntIncrementer(s_cbIntIncr);
-        int result3 = s_testService.IncrementWithCallbackIncrementer(arg3);
+        m_testService.RegisterCallbackIntIncrementer(m_cbIntIncr);
+        int result3 = m_testService.IncrementWithCallbackIncrementer(arg3);
         assertEquals("wrong result 3", arg3 + 1, result3);
     }
 
