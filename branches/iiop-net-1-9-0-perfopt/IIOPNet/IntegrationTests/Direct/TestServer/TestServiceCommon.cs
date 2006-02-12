34123fcee3f6c3c0684f2a15bdd08cd38fec7731
dullmann <dullmann@b113d865-ab28-0410-9ae9-d2690f63996d>
2006-02-12 13:49:44 +0000

    added more integration tests for uint; flags; new enum mapping

--- branches/iiop-net-1-9-0-perfopt/IIOPNet/IntegrationTests/Direct/TestServer/TestServiceCommon.cs
+++ branches/iiop-net-1-9-0-perfopt/IIOPNet/IntegrationTests/Direct/TestServer/TestServiceCommon.cs
@@ -41,6 +41,19 @@ namespace Ch.Elca.Iiop.IntegrationTests {
         A1 = 0x01, B1 = 0x02, C1 = 0x04, D1 = 0x08, All = A1 | B1 | C1 | D1
     }
 
+    public enum TestEnumBI16 : short {
+        A1, B1, C1
+    }
+
+    public enum TestEnumBI64 : long {
+        AL = Int64.MaxValue, BL = 1000
+    }
+
+    public enum TestEnumUI32 : uint {
+        A2, B2 = 10, C2 = 20
+    }
+
+
     [Serializable]
     public struct TestStructA {
         public System.Int32 X;
@@ -158,6 +171,12 @@ namespace Ch.Elca.Iiop.IntegrationTests {
         System.String TestAppendString(System.String basic, System.String toAppend);
 
         TestEnum TestEchoEnumVal(TestEnum arg);
+
+        TestEnumBI16 TestEchoEnumI16Val(TestEnumBI16 arg);
+
+        TestEnumBI64 TestEchoEnumI64Val(TestEnumBI64 arg);        
+
+        TestEnumUI32 TestEchoEnumUI32Val(TestEnumUI32 arg);
 
         TestFlags TestEchoFlagsVal(TestFlags arg);
 
