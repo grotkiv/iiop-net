ceaef9d5918dbf6637ecbdc0d961600fa0260215
dullmann <dullmann@b113d865-ab28-0410-9ae9-d2690f63996d>
2006-02-19 09:05:56 +0000

    refactored for testing with nunitforms

--- branches/iiop-net-1-9-0-perfopt/IIOPNet/Examples/Callback/ChatroomInterDN/Client/Client.cs
+++ branches/iiop-net-1-9-0-perfopt/IIOPNet/Examples/Callback/ChatroomInterDN/Client/Client.cs
@@ -38,40 +38,92 @@ namespace Ch.Elca.Iiop.Demo.Chatroom {
 
     public class Client {
 
+        #region IFields
+
+        private IChatroom m_chatroom;
+        private IiopChannel m_channel;
+
+        #endregion IFields
+        #region IConstructors
+
+        public Client(string[] args) {
+            Setup(args);
+        }
+
+        #endregion IConstructors
+        #region IProperties
+        
+        public IChatroom Chatroom {
+            get {
+                return m_chatroom;
+            }
+        }
+
+        #endregion IProperties
+        #region IMethods
+
+        private void SetupChannel(int callbackPort) {
+            m_channel = new IiopChannel(callbackPort);
+            ChannelServices.RegisterChannel(m_channel);
+        }
+
+        private void RetrieveChatRoom(string nameServiceHost, 
+                                      int nameServicePort) {
+            CorbaInit init = CorbaInit.GetInit();
+            NamingContext nameService = (NamingContext)init.GetNameService(nameServiceHost, nameServicePort);
+
+            NameComponent[] name = new NameComponent[] { new NameComponent("chatroom", "") };
+            // get the chatroom
+            m_chatroom = (IChatroom) nameService.resolve(name);
+        }
+
+        private void Setup(string[] args) {
+            string nameServiceHost = "localhost";
+            int nameServicePort = 8087;
+            if (args.Length > 0) {
+                nameServiceHost = args[0];
+            }
+            if (args.Length > 1) {
+                nameServicePort = Int32.Parse(args[1]);
+            }
+            // the port the callback is listening on
+            int callbackPort = 0; // auto assign
+            if (args.Length > 2) {
+                callbackPort = Int32.Parse(args[2]);
+            }
+            SetupChannel(callbackPort);
+            RetrieveChatRoom(nameServiceHost, nameServicePort);
+        }
+
+        public void TearDown() {
+            if (m_channel != null) {
+                ChannelServices.UnregisterChannel(m_channel);
+                m_channel = null;
+            }
+        }
+
+        public void Run() {
+            try {
+                Application.Run(new Chatform(Chatroom));
+            } finally {
+                TearDown();
+            }
+        }
+
+        #endregion IMethods
+        #region SMethods
+
         [STAThread]
         public static void Main(string[] args) {
-            try {
-
-                string nameServiceHost = "localhost";
-                int nameServicePort = 8087;
-                if (args.Length > 0) {
-                    nameServiceHost = args[0];
-                }
-                if (args.Length > 1) {
-                    nameServicePort = Int32.Parse(args[1]);
-                }
-
-                // the port the callback is listening on
-                int callbackPort = 0; // auto assign
-                if (args.Length > 2) {
-                    callbackPort = Int32.Parse(args[2]);
-                }
-            
-                IiopChannel channel = new IiopChannel(callbackPort);
-                ChannelServices.RegisterChannel(channel);
-
-                CorbaInit init = CorbaInit.GetInit();
-                NamingContext nameService = (NamingContext)init.GetNameService(nameServiceHost, nameServicePort);
-
-                NameComponent[] name = new NameComponent[] { new NameComponent("chatroom", "") };
-                // get the chatroom
-                IChatroom chatroom = (IChatroom) nameService.resolve(name);
-
-                Application.Run(new Chatform(chatroom));
-
+            try {           
+                Client client = new Client(args);
+                client.Run();
             } catch (Exception e) {
                 Console.WriteLine("exception: " + e);
             }
         }
+
+        #endregion SMethods
+
     }
 }
