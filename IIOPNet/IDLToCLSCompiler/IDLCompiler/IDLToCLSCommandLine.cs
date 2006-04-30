1153fccef219ddd889b0568d36c58b88e4828520
dullmann <dullmann@b113d865-ab28-0410-9ae9-d2690f63996d>
2006-04-30 10:45:07 +0000

    refactoring started: separate out command line parsing from IDLToCLS class into separate class IDlToCLSCommandLine

---
+++ IIOPNet/IDLToCLSCompiler/IDLCompiler/IDLToCLSCommandLine.cs
@@ -0,0 +1,189 @@
+/* IDLToCLS.cs
+ * 
+ * Project: IIOP.NET
+ * IDLToCLSCompiler
+ * 
+ * WHEN      RESPONSIBLE
+ * 30.04.06  Dominic Ullmann (DUL), dominic.ullmann -at- elca.ch
+ * 
+ * Copyright 2006 Dominic Ullmann
+ *
+ * Copyright 2003 ELCA Informatique SA
+ * Av. de la Harpe 22-24, 1000 Lausanne 13, Switzerland
+ * www.elca.ch
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using parser;
+using Ch.Elca.Iiop.IdlCompiler.Action;
+using Ch.Elca.Iiop.IdlPreprocessor;
+
+namespace Ch.Elca.Iiop.IdlCompiler {
+
+
+    /// <summary>
+    /// The class responsible for handling the compiler command line.
+    /// </summary>
+    public class IDLToCLSCommandLine {
+        
+        #region IFields
+        
+        private DirectoryInfo m_outputDirectory = new DirectoryInfo(".");
+        private bool m_isInvalid = false;
+        private string m_errorMessage = String.Empty;
+        
+        #endregion IFields
+        #region IConstructors
+        
+        public IDLToCLSCommandLine(string[] args) {
+            ParseArgs(args);
+        }
+        
+        #endregion IConstructors
+        #region IProperties
+        
+        
+        /// <summary>the directory, the output will be written to.</summary>
+        public DirectoryInfo OutputDirectory {
+            get {
+                return m_outputDirectory;
+            }
+        }
+        
+        /// <summary>returns true, if an error has been detected.</summary>
+        public bool IsInvalid {
+            get {
+                return m_isInvalid;
+            }
+        }
+        
+        /// <summary>
+        /// if IsInvalid is true, contains the corresponding error message.
+        /// </summary>
+        public string ErrorMessage {
+            get {
+                return m_errorMessage;
+            }
+        }
+        
+        #endregion IProperties
+        #region IMethods
+        
+        private void ParseArgs(string[] args) {
+            int i = 0;
+
+            while ((i < args.Length) && (args[i].StartsWith("-"))) {
+                if (args[i].Equals("-h")) {
+                
+                    i++;
+                } else if (args[i].Equals("-o")) {
+                    i++;
+                    m_outputDirectory = new DirectoryInfo(args[i++]);
+                } else if (args[i].StartsWith("-out:")) {                    
+                    m_outputDirectory = new DirectoryInfo(args[i].Substring(5));
+                    i++;
+                } else {
+                    m_isInvalid = true;
+                    m_errorMessage =
+                        String.Format("Error: invalid option {0}", args[i]);
+                    return;
+                }                
+            }
+            
+        }
+        
+        #endregion IMethods
+                                
+    }
+    
+}
+
+
+#if UnitTest
+
+
+namespace Ch.Elca.Iiop.IdlCompiler.Tests {
+	
+    using System;
+    using System.Reflection;
+    using System.Collections;
+    using System.IO;
+    using System.Text;
+    using NUnit.Framework;
+    using Ch.Elca.Iiop.IdlCompiler;
+
+    /// <summary>
+    /// Unit-tests for the IDLToCLS CommandLine handling.
+    /// </summary>
+    [TestFixture]
+    public class IDLToCLSCommandLineTest {
+                        
+        
+        [Test]
+        public void TestDefaultOutputDir() {
+            DirectoryInfo testDir = new DirectoryInfo(".");
+            IDLToCLSCommandLine commandLine = new IDLToCLSCommandLine(new string[0]);
+            Assertion.AssertEquals("OutputDirectory", testDir.FullName,
+                                   commandLine.OutputDirectory.FullName);            
+        }
+        
+        [Test]
+        public void TestOutDirSpaceSeparator() {            
+            DirectoryInfo testDir = new DirectoryInfo(Path.Combine(".", "testOut"));
+            IDLToCLSCommandLine commandLine = new IDLToCLSCommandLine(
+                new string[] { "-o", testDir.FullName});
+            Assertion.AssertEquals("OutputDirectory", testDir.FullName,
+                                   commandLine.OutputDirectory.FullName);
+        }
+        
+        [Test]
+        public void TestOutDirColonSeparator() {
+            DirectoryInfo testDir = new DirectoryInfo(Path.Combine(".", "testOut"));
+            IDLToCLSCommandLine commandLine = new IDLToCLSCommandLine(
+                new string[] { "-out:" + testDir.FullName});
+            Assertion.AssertEquals("OutputDirectory", testDir.FullName,
+                                   commandLine.OutputDirectory.FullName);
+        }
+        
+        [Test]
+        public void TestWrongArgument() {
+            IDLToCLSCommandLine commandLine = new IDLToCLSCommandLine(
+                new string[] { "-InvalidArg"} );
+            Assertion.Assert("Invalid Arg detection",
+                             commandLine.IsInvalid);
+            Assertion.AssertEquals("invalid arguments message",
+                                   "Error: invalid option -InvalidArg",
+                                   commandLine.ErrorMessage);
+        }
+        
+        
+    }
+}
+
+#endif
+ 
+
